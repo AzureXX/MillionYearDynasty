fd0f7d5bd75e06b1e344100b3011b1d961e8003a
AzureXX <ismat.valiyev@gmail.com>
2019-01-10 19:38:23 +0400

    update

--- Assets/Scripts/Map/Chunk.cs
+++ Assets/Scripts/Map/Chunk.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
+    [SerializeField] ChunkStats stats;
+    [SerializeField] List<Tile> chunks = new List<Tile>();
+
     // Start is called before the first frame update
     void Start()
     {
